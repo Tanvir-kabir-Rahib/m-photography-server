720b7b20e7cb7bab8bec7fda636a57edb7c10355
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-10 19:46:00 +0600

    services api added

--- index.js
+++ index.js
@@ -57,6 +57,20 @@ async function run() {
             res.send(banners);
         });
   
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+  
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+  
         
     }
     finally {
