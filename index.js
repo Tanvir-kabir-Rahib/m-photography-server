a5e47a1f8bf0e47f9c77cb6becfe1f542452dcab
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-10 19:12:44 +0600

    collections added

--- index.js
+++ index.js
@@ -13,22 +13,44 @@ app.get('/', (req, res) => {
     res.send("m-server is running")
 })
 
-function verifyJWT(req, res, next){
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.thwifey.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'});
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-        if(err){
-            return res.status(403).send({message: 'Forbidden access'});
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
         }
         req.decoded = decoded;
         next();
     })
 }
+async function run() {
+    try {
+        const serviceCollection = client.db('mPhotography').collection('services');
+        const bannerCollection = client.db('mPhotography').collection('banners');
+        const reviewCollection = client.db('mPhotography').collection('reviews');
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.error(err));
+
 
 app.listen(port, () => {
     console.log("m-server is running on port", port)
