80263604087a012f552c9fc3c066cd8edccbd6a1
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-10 19:31:38 +0600

    initial

--- index.js
+++ index.js
@@ -37,19 +37,7 @@ function verifyJWT(req, res, next) {
         next();
     })
 }
-async function run() {
-    try {
-        const serviceCollection = client.db('mPhotography').collection('services');
-        const bannerCollection = client.db('mPhotography').collection('banners');
-        const reviewCollection = client.db('mPhotography').collection('reviews');
 
-    }
-    finally {
-
-    }
-
-}
-run().catch(err => console.error(err));
 
 
 app.listen(port, () => {
