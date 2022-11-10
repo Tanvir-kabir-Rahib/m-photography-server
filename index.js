686b62c359256275e26ebcfa3e26c56f7ec1e046
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-10 09:47:51 +0600

    Verify jwt function added

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 4000;
+
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send("m-server is running")
 })
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 app.listen(port, () => {
     console.log("m-server is running on port", port)
